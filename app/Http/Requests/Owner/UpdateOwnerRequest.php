abbead6efd8c7f5bfa3cf4099e2989a27e8c478a
Flavio Saldanha <tminemcraft123@gmail.com>
2025-05-31 23:16:49 -0300

    refactor: Remove Authorize from Owner Request to user only middlewares

--- app/Http/Requests/Owner/UpdateOwnerRequest.php
+++ app/Http/Requests/Owner/UpdateOwnerRequest.php
@@ -7,11 +7,6 @@ use Illuminate\Validation\Rule;
 
 class UpdateOwnerRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     public function rules(): array
     {
         return [
